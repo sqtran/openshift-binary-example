8cc2f73d302e12973bc2f7e1b206f82b07518c19
Steve Tran <sqtran@users.noreply.github.com>
2016-02-16 12:40:10 -0500

    Added a debugging line
    Checking to see if this file is even being called.

--- .sti/bin/build.sh
+++ .sti/bin/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Hello Steve!! This file is being executed by some build tool...."
 
 # Download and place binary in JBoss. WAR_FILE_URL variable is specified as an environment variable in the BuildConfig
 curl -o $JBOSS_HOME/standalone/deployments/ROOT.war -O ${WAR_FILE_URL}
